b3a40333cf2df3f50a8dd72b1cb3bebbcfceaa29
Joilson Rodrigues <62257507+joilson-dev@users.noreply.github.com>
2024-09-29 00:22:28 -0300

    feat: implement GET item by ID route with validation

--- src/app.js
+++ src/app.js
@@ -5,61 +5,75 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const items = [{
-    name: "Uva",
-    quantity: 2,
-    type: "fruta"
-},
-{
-    name: "Maçã",
-    quantity: 5,
-    type: "fruta"
-},
-{
-    name: "Banana",
-    quantity: 6,
-    type: "fruta"
-},
-{
-    name: "Pão",
-    quantity: 1,
-    type: "padaria"
-},
-{
-    name: "Bolo",
-    quantity: 1,
-    type: "padaria"
-},
-{
-    name: "Leite",
-    quantity: 3,
-    type: "laticínios"
-},
-{
-    name: "Queijo",
-    quantity: 1,
-    type: "laticínios"
-},
-{
-    name: "Ovo",
-    quantity: 12,
-    type: "proteína"
-},
-{
-    name: "Frango",
-    quantity: 1,
-    type: "proteína"
-},
-{
-    name: "Arroz",
-    quantity: 1,
-    type: "grãos"
-},
-{
-    name: "Feijão",
-    quantity: 1,
-    type: "grãos"
-}]
+const items = [
+    {
+        id: 1,
+        name: "Uva",
+        quantity: 2,
+        type: "fruta"
+    },
+    {
+        id: 2,
+        name: "Maçã",
+        quantity: 5,
+        type: "fruta"
+    },
+    {
+        id: 3,
+        name: "Banana",
+        quantity: 6,
+        type: "fruta"
+    },
+    {
+        id: 4,
+        name: "Pão",
+        quantity: 1,
+        type: "padaria"
+    },
+    {
+        id: 5,
+        name: "Bolo",
+        quantity: 1,
+        type: "padaria"
+    },
+    {
+        id: 6,
+        name: "Leite",
+        quantity: 3,
+        type: "laticínios"
+    },
+    {
+        id: 7,
+        name: "Queijo",
+        quantity: 1,
+        type: "laticínios"
+    },
+    {
+        id: 8,
+        name: "Ovo",
+        quantity: 12,
+        type: "proteína"
+    },
+    {
+        id: 9,
+        name: "Frango",
+        quantity: 1,
+        type: "proteína"
+    },
+    {
+        id: 10,
+        name: "Arroz",
+        quantity: 1,
+        type: "grãos"
+    },
+    {
+        id: 11,
+        name: "Feijão",
+        quantity: 1,
+        type: "grãos"
+    }
+];
+
 
 app.post('/items', (req, res) => {
     const { name, quantity, type } = req.body
@@ -100,6 +114,21 @@ app.get('/items', (req, res) => {
     }
 
     return res.status(200).send(items);
+});
+
+app.get('/items/:id', (req, res) => {
+    const { id } = req.params;
+    const idNumber = parseInt(id, 10);
+    if (isNaN(idNumber) || idNumber < 1 || idNumber > items.length) {
+        return res.status(400).send('ID inválido!');
+    }
+
+    const item = items.find(item => item.id === idNumber);
+    if (!item) {
+        return res.status(404).send('Item não encontrado.');
+    }
+
+    return res.status(200).send(item);
 });
 
 app.listen(5000, () => {
